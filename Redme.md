4275d0b80fde4653aabd65714fa94e66e58f935b
咕谷酱 <74496778+GooGuJiang@users.noreply.github.com>
2022-05-15 14:52:11 +0800

    更新表情

---
+++ Redme.md
@@ -0,0 +1,10 @@
+# Gu-Stickers
+一个用于添加 argon WordPress 主题表情的插件
+
+# 目前标签缓慢更新中....
+
+## 已经添加表情
+- 八寻宁宁
+
+# 展示图
+[![O2tJhj.png](https://s1.ax1x.com/2022/05/15/O2tJhj.png)](https://imgtu.com/i/O2tJhj)
--- gu-stickers.php
+++ gu-stickers.php
@@ -5,14 +5,60 @@ Description: 咕谷的私用表情
 Version: 1.0.1
 */
     function add_more_emotions($emotionList){
+        $gu_img_url = WP_PLUGIN_URL."/".dirname(plugin_basename(__FILE__));
         array_push(
             $emotionList,
             array(
-                'groupname' => '还没添加呢!', 
+                'groupname' => '八寻宁宁', 
                 'list' => array(
-                    array('type' => 'sticker', 'code' => 'test-1', 'src' => $GLOBALS['assets_path'] . '/stickers/test/1.jpg'),
-                    array('type' => 'sticker', 'code' => 'test-2', 'src' => $GLOBALS['assets_path'] . '/stickers/test/2.jpg'),
-                    array('type' => 'sticker', 'code' => 'test-3', 'src' => $GLOBALS['assets_path'] . '/stickers/test/3.jpg')
+                    array('type' => 'sticker', 'code' => 'YashiroNene-1', 'src' => $gu_img_url . '/stickers/YashiroNene/1.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-2', 'src' => $gu_img_url . '/stickers/YashiroNene/2.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-3', 'src' => $gu_img_url . '/stickers/YashiroNene/3.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-4', 'src' => $gu_img_url . '/stickers/YashiroNene/4.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-5', 'src' => $gu_img_url . '/stickers/YashiroNene/5.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-6', 'src' => $gu_img_url . '/stickers/YashiroNene/6.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-7', 'src' => $gu_img_url . '/stickers/YashiroNene/7.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-8', 'src' => $gu_img_url . '/stickers/YashiroNene/8.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-9', 'src' => $gu_img_url . '/stickers/YashiroNene/9.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-10', 'src' => $gu_img_url . '/stickers/YashiroNene/10.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-11', 'src' => $gu_img_url . '/stickers/YashiroNene/11.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-12', 'src' => $gu_img_url . '/stickers/YashiroNene/12.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-13', 'src' => $gu_img_url . '/stickers/YashiroNene/13.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-14', 'src' => $gu_img_url . '/stickers/YashiroNene/14.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-15', 'src' => $gu_img_url . '/stickers/YashiroNene/15.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-16', 'src' => $gu_img_url . '/stickers/YashiroNene/16.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-17', 'src' => $gu_img_url . '/stickers/YashiroNene/17.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-18', 'src' => $gu_img_url . '/stickers/YashiroNene/18.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-19', 'src' => $gu_img_url . '/stickers/YashiroNene/19.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-20', 'src' => $gu_img_url . '/stickers/YashiroNene/20.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-21', 'src' => $gu_img_url . '/stickers/YashiroNene/21.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-22', 'src' => $gu_img_url . '/stickers/YashiroNene/22.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-23', 'src' => $gu_img_url . '/stickers/YashiroNene/23.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-24', 'src' => $gu_img_url . '/stickers/YashiroNene/24.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-25', 'src' => $gu_img_url . '/stickers/YashiroNene/25.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-26', 'src' => $gu_img_url . '/stickers/YashiroNene/26.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-27', 'src' => $gu_img_url . '/stickers/YashiroNene/27.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-28', 'src' => $gu_img_url . '/stickers/YashiroNene/28.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-29', 'src' => $gu_img_url . '/stickers/YashiroNene/29.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-30', 'src' => $gu_img_url . '/stickers/YashiroNene/30.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-31', 'src' => $gu_img_url . '/stickers/YashiroNene/31.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-32', 'src' => $gu_img_url . '/stickers/YashiroNene/32.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-33', 'src' => $gu_img_url . '/stickers/YashiroNene/33.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-34', 'src' => $gu_img_url . '/stickers/YashiroNene/34.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-35', 'src' => $gu_img_url . '/stickers/YashiroNene/35.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-36', 'src' => $gu_img_url . '/stickers/YashiroNene/36.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-37', 'src' => $gu_img_url . '/stickers/YashiroNene/37.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-38', 'src' => $gu_img_url . '/stickers/YashiroNene/38.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-39', 'src' => $gu_img_url . '/stickers/YashiroNene/39.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-40', 'src' => $gu_img_url . '/stickers/YashiroNene/40.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-41', 'src' => $gu_img_url . '/stickers/YashiroNene/41.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-42', 'src' => $gu_img_url . '/stickers/YashiroNene/42.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-43', 'src' => $gu_img_url . '/stickers/YashiroNene/43.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-44', 'src' => $gu_img_url . '/stickers/YashiroNene/44.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-45', 'src' => $gu_img_url . '/stickers/YashiroNene/45.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-46', 'src' => $gu_img_url . '/stickers/YashiroNene/46.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-47', 'src' => $gu_img_url . '/stickers/YashiroNene/47.webp'),
+                    array('type' => 'sticker', 'code' => 'YashiroNene-48', 'src' => $gu_img_url . '/stickers/YashiroNene/48.webp')
                 )
             )
         );
